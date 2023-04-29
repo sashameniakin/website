56ec3a2b83d6b4f9a54d58edbb0d7fb6ebc86918
Oleksandr Meniakin <oleksandr.meniakin@gmail.com>
2023-04-29 18:16:36 +0200

    init commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,10 @@
+export default function Nav() {
+  return (
+    <>
+      <div>
+        <img src="./images/background.png" alt="background" />
+        <p>jhgkjhfgljg</p>
+      </div>
+    </>
+  );
+}
