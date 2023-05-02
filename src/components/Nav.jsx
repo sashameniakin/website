77133873bd429349d6cbb41423ada11f326c3393
Oleksandr Meniakin <oleksandr.meniakin@gmail.com>
2023-05-02 21:12:31 +0200

    only about part not done

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,14 @@
 import Typewriter from "typewriter-effect";
 
-export default function Nav() {
+export default function Nav({ section2InView, section2, section1InView }) {
   return (
     <>
-      <section className="bg-[url('./assets/images/background.png')] bg-cover h-screen bg-no-repeat bg-center opacity-80 flex flex-col">
+      <section
+        ref={section2}
+        className="bg-[url('./assets/images/background.png')] bg-cover h-screen bg-no-repeat bg-center opacity-80 flex flex-col"
+      >
         <header className="h-screen">
-          <nav className="flex justify-between p-8">
+          <nav className="flex justify-between p-8 fixed top-0 left-0 right-0">
             <div className="flex gap-5">
               <a
                 href="https://github.com/sashameniakin"
@@ -14,14 +17,21 @@ export default function Nav() {
               >
                 <img
                   className="w-[22px] h-[22px] hover:cursor-pointer"
-                  src="./images/icons/icon-github.svg"
+                  src={`${
+                    section1InView && !section2InView
+                      ? "./images/icons/icon-github-black.svg"
+                      : "./images/icons/icon-github.svg"
+                  }`}
                   alt="github"
                   onMouseOver={(e) =>
                     (e.currentTarget.src =
                       "./images/icons/icon-github-active.svg")
                   }
                   onMouseOut={(e) =>
-                    (e.currentTarget.src = "./images/icons/icon-github.svg")
+                    section1InView && !section2InView
+                      ? (e.currentTarget.src =
+                          "./images/icons/icon-github-black.svg")
+                      : (e.currentTarget.src = "./images/icons/icon-github.svg")
                   }
                 />
               </a>
@@ -32,15 +42,22 @@ export default function Nav() {
               >
                 <img
                   className="w-[22px] h-[22px] hover:cursor-pointer"
-                  src="./images/icons/icon-frontend-mentor.svg"
+                  src={`${
+                    section1InView && !section2InView
+                      ? "./images/icons/icon-frontend-mentor-black.svg"
+                      : "./images/icons/icon-frontend-mentor.svg"
+                  }`}
                   alt="frontend-mentor"
                   onMouseOver={(e) =>
                     (e.currentTarget.src =
                       "./images/icons/icon-frontend-mentor-active.svg")
                   }
                   onMouseOut={(e) =>
-                    (e.currentTarget.src =
-                      "./images/icons/icon-frontend-mentor.svg")
+                    section1InView && !section2InView
+                      ? (e.currentTarget.src =
+                          "./images/icons/icon-frontend-mentor-black.svg")
+                      : (e.currentTarget.src =
+                          "./images/icons/icon-frontend-mentor.svg")
                   }
                 />
               </a>
@@ -51,34 +68,59 @@ export default function Nav() {
               >
                 <img
                   className="w-[22px] h-[22px] cursor-pointer"
-                  src="./images/icons/icon-linkedin.svg"
+                  src={`${
+                    section1InView && !section2InView
+                      ? "./images/icons/icon-linkedin-black.svg"
+                      : "./images/icons/icon-linkedin.svg"
+                  }`}
                   alt="linkedin"
                   onMouseOver={(e) =>
                     (e.currentTarget.src =
                       "./images/icons/icon-linkedin-active.svg")
                   }
                   onMouseOut={(e) =>
-                    (e.currentTarget.src = "./images/icons/icon-linkedin.svg")
+                    section1InView && !section2InView
+                      ? (e.currentTarget.src =
+                          "./images/icons/icon-linkedin-black.svg")
+                      : (e.currentTarget.src =
+                          "./images/icons/icon-linkedin.svg")
                   }
                 />
               </a>
             </div>
-            <div className="flex text-pureWhite gap-6 text-nav">
-              <p className="hover:text-secondary hover:cursor-pointer hover:underline decoration-secondary decoration-2 underline-offset-[5px]">
-                HOME
-              </p>
-              <p className="hover:text-secondary hover:cursor-pointer hover:underline decoration-secondary decoration-2 underline-offset-[5px]">
-                ABOUT
-              </p>
-              <p className="hover:text-secondary hover:cursor-pointer hover:underline decoration-secondary decoration-2 underline-offset-[5px]">
-                PROJECTS
-              </p>
-              <p className="hover:text-secondary hover:cursor-pointer hover:underline decoration-secondary decoration-2 underline-offset-[5px]">
-                CONTACT
-              </p>
+            <div
+              className={`flex ${
+                !section1InView && !section2InView ? "flex-col" : "flex-row"
+              } gap-6 text-nav ${
+                section1InView && !section2InView
+                  ? "text-background"
+                  : "text-pureWhite"
+              }`}
+            >
+              <a href="#home">
+                <p className="hover:text-secondary hover:cursor-pointer hover:underline decoration-secondary decoration-2 underline-offset-[5px]">
+                  HOME
+                </p>
+              </a>
+              <a href="#about">
+                <p className="hover:text-secondary hover:cursor-pointer hover:underline decoration-secondary decoration-2 underline-offset-[5px]">
+                  ABOUT
+                </p>
+              </a>
+              <a href="#projects">
+                <p className="hover:text-secondary hover:cursor-pointer hover:underline decoration-secondary decoration-2 underline-offset-[5px]">
+                  PROJECTS
+                </p>
+              </a>
+              <a href="#contact">
+                <p className="hover:text-secondary hover:cursor-pointer hover:underline decoration-secondary decoration-2 underline-offset-[5px]">
+                  CONTACT
+                </p>
+              </a>
             </div>
           </nav>
-          <div className="flex flex-col items-end text-pureWhite px-40 pt-[60px] text-h2">
+
+          <div className="flex flex-col items-end text-pureWhite px-40 pt-[140px] text-h2">
             <div className="flex flex-col items-start gap-5">
               <h1 className="text-h1">WELCOME TO MY WEBSITE</h1>
               <p>
@@ -102,9 +144,10 @@ export default function Nav() {
             </div>
           </div>
         </header>
-        <div className="justify-self-center self-center w-[30px] h-[48px] border-solid border-2 rounded-[60px] animate-bounce border-pureWhite before:w-[7px] before:h-[7px] before:border-pureWhite before:border-2 before:rounded-[60px] before:absolute before:top-[10px] before:left-[10px] before:bg-pureWhite before:animate-bounce" />
+        <a className="justify-self-center self-center" href="#about">
+          <div className=" w-[30px] h-[48px] border-solid border-2 rounded-[60px] animate-bounce border-pureWhite before:w-[7px] before:h-[7px] before:border-pureWhite before:border-2 before:rounded-[60px] before:absolute before:top-[10px] before:left-[10px] before:bg-pureWhite before:animate-bounce" />
+        </a>
       </section>
-      <img src="./images/projects/preview.jpg" alt="foto" />
     </>
   );
 }
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,17 +1,23 @@
 export default function Project({ src_large, src_small, name, techstack }) {
   return (
     <section className="flex flex-col justify-start">
-      <picture className="relative flex justify-center">
-        {/* <source media="(min-width:415px)" srcSet={src_large} /> */}
+      <picture className="group relative flex justify-center">
+        <source media="(min-width:415px)" srcSet={src_large} />
         <img src={src_large} alt="foto" />
-        <p>VIEW PROJECT</p>
-        <p>VIEW CODE</p>
-        <span className="hover:absolute hover:top-0 hover:w-full hover:h-full hover:bg-black hover:opacity-70" />
+        <div className="hidden absolute z-10 h-full items-center justify-center gap-5 group-hover:flex group-hover:flex-col">
+          <p className="text-pureWhite text-dev hover:text-secondary hover:cursor-pointer underline decoration-secondary decoration-2 underline-offset-[5px]">
+            VIEW PROJECT
+          </p>
+          <p className="text-pureWhite text-dev hover:text-secondary hover:cursor-pointer underline decoration-secondary decoration-2 underline-offset-[5px]">
+            VIEW CODE
+          </p>
+        </div>
+        <span className="group-hover:absolute group-hover:top-0 group-hover:w-full group-hover:h-full group-hover:bg-black group-hover:opacity-70 group-hover:cursor-pointer" />
       </picture>
 
-      <p>{name}</p>
-      <p>{techstack}</p>
-      <div className="hidden justify-start gap-[10px]">
+      <p className="text-pureWhite text-projects pt-4">{name}</p>
+      <p className="text-pureWhite text-h3 py-2">{techstack}</p>
+      <div className="hidden justify-start gap-[10px] group-hover:flex">
         <p className="hidden absolute">VIEW PROJECT</p>
         <p className="hidden absolute">VIEW CODE</p>
       </div>
