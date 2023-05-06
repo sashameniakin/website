f5b685e47cae6cd5d51d6964aee048fa86264177
Oleksandr Meniakin <oleksandr.meniakin@gmail.com>
2023-05-06 23:26:31 +0200

    about part

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,6 +3,9 @@ import ScrollUpButton from "./UI/ScrollUpButton";
 export default function About({ section1 }) {
   return (
     <div ref={section1} className="h-[500px] bg-whiteBackground" id="about">
+      <div className="flex w-[80%] mx-auto pt-10">
+        <h1 className="text-contact text-background">ABOUT</h1>
+      </div>
       <div className="flex justify-center items-center h-full">
         <p className="text-h2 text-background">under construction</p>
       </div>
